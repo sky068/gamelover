29e79c8cfedec0ec735322df61ac87bc7ef240b3
xujiawei <xujiawei@puton-inc.com>
2020-03-27 17:43:19 +0800

    添加卡片连接demo

---
+++ games/drawline/src/project.dev.js
@@ -0,0 +1,250 @@
+window.__require = function e(t, n, r) {
+  function s(o, u) {
+    if (!n[o]) {
+      if (!t[o]) {
+        var b = o.split("/");
+        b = b[b.length - 1];
+        if (!t[b]) {
+          var a = "function" == typeof __require && __require;
+          if (!u && a) return a(b, !0);
+          if (i) return i(b, !0);
+          throw new Error("Cannot find module '" + o + "'");
+        }
+      }
+      var f = n[o] = {
+        exports: {}
+      };
+      t[o][0].call(f.exports, function(e) {
+        var n = t[o][1][e];
+        return s(n || e);
+      }, f, f.exports, e, t, n, r);
+    }
+    return n[o].exports;
+  }
+  var i = "function" == typeof __require && __require;
+  for (var o = 0; o < r.length; o++) s(r[o]);
+  return s;
+}({
+  AnswerCardCtrl: [ function(require, module, exports) {
+    "use strict";
+    cc._RF.push(module, "213d3rVkKFHLaiH2RxUhqwv", "AnswerCardCtrl");
+    "use strict";
+    cc.Class({
+      extends: cc.Component,
+      properties: {
+        contentLabel: cc.Label,
+        content: {
+          default: "",
+          notify: function notify() {
+            this.contentLabel.string = this.content;
+          }
+        }
+      }
+    });
+    cc._RF.pop();
+  }, {} ],
+  Game: [ function(require, module, exports) {
+    "use strict";
+    cc._RF.push(module, "29112Z6C4ZM5pLasAbq3c6d", "Game");
+    "use strict";
+    var questions = [ {
+      url: "",
+      answer: "hello"
+    }, {
+      url: "",
+      answer: "world"
+    }, {
+      url: "",
+      answer: "test"
+    }, {
+      url: "",
+      answer: "aa"
+    } ];
+    var answers = [ "hello", "world", "test", "aa" ];
+    var LineCtrl = require("./LineCtrl.js");
+    var QuestionCardCtrl = require("./QuestionCardCtrl");
+    var AnswerCardCtrl = require("./AnswerCardCtrl");
+    cc.Class({
+      extends: cc.Component,
+      properties: {
+        linePF: cc.Prefab,
+        graphics: cc.Graphics,
+        qCards: [ cc.Node ],
+        aCards: [ cc.Node ]
+      },
+      start: function start() {
+        this.canvas = cc.director.getScene().getChildByName("Canvas");
+        this.node.on(cc.Node.EventType.TOUCH_START, this.touchStart, this);
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, this.touchMove, this);
+        this.node.on(cc.Node.EventType.TOUCH_END, this.touchEnd, this);
+        this.node.on(cc.Node.EventType.TOUCH_END, this.touchCancle, this);
+        this.init();
+      },
+      init: function init() {
+        for (var i = 0; i < this.qCards.length; i++) this.qCards[i].getComponent(QuestionCardCtrl).answer = questions[i].answer;
+        for (var _i = 0; _i < this.aCards.length; _i++) this.aCards[_i].getComponent(AnswerCardCtrl).content = answers[_i];
+        this.startCard = null;
+        this.lines = [];
+      },
+      test: function test() {
+        var startNode = this.qCards[0];
+        var startPos = startNode.getPosition();
+        startPos.y -= startNode.height / 2;
+        var endNode = this.aCards[2];
+        var endPos = endNode.getPosition();
+        endPos.y += endNode.height / 2;
+        var line = cc.instantiate(this.linePF);
+        line.parent = startNode.parent;
+        var lineCtrl = line.getComponent(LineCtrl);
+        lineCtrl.init(startPos, endPos);
+      },
+      touchStart: function touchStart(event) {
+        var point = event.getLocation();
+        var card = this._getTouchedCard(this.qCards, point);
+        this.startCard = card || null;
+      },
+      touchMove: function touchMove(event) {},
+      touchEnd: function touchEnd(event) {
+        var _this = this;
+        var point = event.getLocation();
+        var endCard = this._getTouchedCard(this.aCards, point);
+        if (this.startCard && endCard) {
+          cc.log("\u5212\u7ebf\u5f00\u59cb");
+          if (this.startCard.line && cc.isValid(this.startCard.line, true)) {
+            this._rmEleInArr(this.lines, this.startCard.line);
+            this.startCard.line.destroy();
+          }
+          if (endCard.line && cc.isValid(endCard.line, true)) {
+            this._rmEleInArr(this.lines, endCard.line);
+            endCard.line.destroy();
+          }
+          var startPos = this.startCard.getPosition();
+          startPos.y -= this.startCard.height / 2;
+          var endPos = endCard.getPosition();
+          endPos.y += endCard.height / 2;
+          var line = cc.instantiate(this.linePF);
+          line.parent = this.canvas;
+          this.startCard.line = line;
+          endCard.line = line;
+          line.start = this.startCard;
+          line.end = endCard;
+          this.lines.push(line);
+          var lineCtrl = line.getComponent(LineCtrl);
+          lineCtrl.init(startPos, endPos);
+          this.lines.length == this.qCards.length && this.scheduleOnce(function() {
+            cc.log("\u8fde\u7ebf\u7ed3\u675f");
+            var ret = _this.checkResult();
+            cc.log("ret:", ret);
+            cc.sys.isBrowser && alert(ret ? "\u6210\u529f" : "\u5931\u8d25");
+          }, .5);
+        }
+      },
+      _getTouchedCard: function _getTouchedCard(cards, touchPoint) {
+        var point = this.canvas.convertToNodeSpaceAR(touchPoint);
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = void 0;
+        try {
+          for (var _iterator = cards[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var card = _step.value;
+            var rect = card.getBoundingBox();
+            if (rect.contains(point)) return card;
+          }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
+        } finally {
+          try {
+            !_iteratorNormalCompletion && _iterator.return && _iterator.return();
+          } finally {
+            if (_didIteratorError) throw _iteratorError;
+          }
+        }
+      },
+      _rmEleInArr: function _rmEleInArr(arr, ele) {
+        var index = arr.indexOf(ele);
+        index >= 0 && arr.splice(index, 1);
+        return arr;
+      },
+      touchCancle: function touchCancle(event) {},
+      checkResult: function checkResult() {
+        var ret = true;
+        if (this.lines.length < this.qCards.length) ret = false; else {
+          var _iteratorNormalCompletion2 = true;
+          var _didIteratorError2 = false;
+          var _iteratorError2 = void 0;
+          try {
+            for (var _iterator2 = this.lines[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+              var line = _step2.value;
+              var question = line.start.getComponent(QuestionCardCtrl);
+              var answer = line.end.getComponent(AnswerCardCtrl);
+              if (question.answer != answer.content) {
+                ret = false;
+                break;
+              }
+            }
+          } catch (err) {
+            _didIteratorError2 = true;
+            _iteratorError2 = err;
+          } finally {
+            try {
+              !_iteratorNormalCompletion2 && _iterator2.return && _iterator2.return();
+            } finally {
+              if (_didIteratorError2) throw _iteratorError2;
+            }
+          }
+        }
+        return ret;
+      }
+    });
+    cc._RF.pop();
+  }, {
+    "./AnswerCardCtrl": "AnswerCardCtrl",
+    "./LineCtrl.js": "LineCtrl.js",
+    "./QuestionCardCtrl": "QuestionCardCtrl"
+  } ],
+  "LineCtrl.js": [ function(require, module, exports) {
+    "use strict";
+    cc._RF.push(module, "3e89diF0BpABqQSbfC1bpbS", "LineCtrl.js");
+    "use strict";
+    cc.Class({
+      extends: cc.Component,
+      properties: {},
+      start: function start() {},
+      init: function init(startPos, endPos) {
+        var lineW = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : 6;
+        this.node.position = startPos;
+        var p = endPos.sub(startPos);
+        var len = p.mag();
+        var pn = p.normalizeSelf();
+        var origin = cc.v2(0, 1);
+        var r = origin.signAngle(pn);
+        var angle = cc.misc.radiansToDegrees(r);
+        this.node.angle = angle - 180;
+        this.node.height = len;
+        this.node.width = lineW;
+      }
+    });
+    cc._RF.pop();
+  }, {} ],
+  QuestionCardCtrl: [ function(require, module, exports) {
+    "use strict";
+    cc._RF.push(module, "0c48d9ShadGy75oj/0FMrJ0", "QuestionCardCtrl");
+    "use strict";
+    cc.Class({
+      extends: cc.Component,
+      properties: {
+        url: {
+          default: "",
+          notify: function notify() {
+            this.updateUI();
+          }
+        },
+        answer: ""
+      },
+      start: function start() {},
+      updateUI: function updateUI() {}
+    });
+    cc._RF.pop();
+  }, {} ]
+}, {}, [ "AnswerCardCtrl", "Game", "LineCtrl.js", "QuestionCardCtrl" ]);
---
+++ games/drawline/src/settings.js
@@ -0,0 +1,108 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {},
+        internal: {
+            "14TDKXr2NJ6LjvHPops74o": [
+                "effects/builtin-2d-gray-sprite.effect",
+                "cc.EffectAsset"
+            ],
+            "0ek66qC1NOQLjgYmi04HvX": [
+                "effects/builtin-2d-spine.effect",
+                "cc.EffectAsset"
+            ],
+            "28dPjdQWxEQIG3VVl1Qm6T": [
+                "effects/builtin-2d-sprite.effect",
+                "cc.EffectAsset"
+            ],
+            c0BAyVxX9JzZy8EjFrc9DU: [
+                "effects/builtin-clear-stencil.effect",
+                "cc.EffectAsset"
+            ],
+            "796vrvt+9F2Zw/WR3INvx6": [
+                "effects/builtin-unlit-transparent.effect",
+                "cc.EffectAsset"
+            ],
+            "6dkeWRTOBGXICfYQ7JUBnG": [
+                "effects/builtin-unlit.effect",
+                "cc.EffectAsset"
+            ],
+            "6fgBCSDDdPMInvyNlggls2": [
+                "materials/builtin-2d-base.mtl",
+                "cc.Material"
+            ],
+            "3ae7efMv1CLq2ilvUY/tQi": [
+                "materials/builtin-2d-gray-sprite.mtl",
+                "cc.Material"
+            ],
+            "7a/QZLET9IDreTiBfRn2PD": [
+                "materials/builtin-2d-spine.mtl",
+                "cc.Material"
+            ],
+            "ecpdLyjvZBwrvm+cedCcQy": [
+                "materials/builtin-2d-sprite.mtl",
+                "cc.Material"
+            ],
+            cffgu4qBxEqa150o1DmRAy: [
+                "materials/builtin-clear-stencil.mtl",
+                "cc.Material"
+            ],
+            "2aKWBXJHxKHLvrBUi2yYZQ": [
+                "materials/builtin-unlit.mtl",
+                "cc.Material"
+            ]
+        }
+    },
+    launchScene: "db://assets/Scene/game.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/game.fire",
+            uuid: "2aPy5EV4xOy7TxDisMRAe+"
+        }
+    ],
+    packedAssets: {
+        "074a85ec0": [
+            "02delMVqdBD70a/HSD99FK",
+            "06eRfYXs1DNq2p6bFjvngG",
+            "e00tY2N+9GAbqbo3k7MQac",
+            "e3Vt+qMgJCFK8BC+kNuIz6"
+        ],
+        "079499991": [
+            "2aKWBXJHxKHLvrBUi2yYZQ",
+            "6dkeWRTOBGXICfYQ7JUBnG"
+        ],
+        "07ce7530a": [
+            "14TDKXr2NJ6LjvHPops74o",
+            "3ae7efMv1CLq2ilvUY/tQi"
+        ],
+        "0818f3bb8": [
+            "07sHoH38dGqpWthr6PE2C3",
+            "2aPy5EV4xOy7TxDisMRAe+",
+            "39wowJmiZHTrSZLCx0vQ46",
+            "94dmQu8u5L2Je16biK8qbu",
+            "a3pYVTeD5PeoyHKo+4Wbsl",
+            "b34Poo7ZdGpIqW/GS50js8",
+            "e85LRpsRNOsYr22WMx8nm8"
+        ],
+        "0d669730c": [
+            "c0BAyVxX9JzZy8EjFrc9DU",
+            "cffgu4qBxEqa150o1DmRAy"
+        ],
+        "0e4bc3b03": [
+            "0ek66qC1NOQLjgYmi04HvX",
+            "7a/QZLET9IDreTiBfRn2PD"
+        ]
+    },
+    md5AssetsMap: {},
+    orientation: "landscape",
+    debug: true,
+    subpackages: {}
+};
